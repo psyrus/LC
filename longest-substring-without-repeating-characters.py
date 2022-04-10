9bcccf9c2675e57580b2643afea7c3d7860d9e55
Trevor Chinn <revolution.x@gmail.com>
2022-04-10 16:35:41 +0900

    Add a double pointer approach

--- longest-substring-without-repeating-characters.py
+++ longest-substring-without-repeating-characters.py
@@ -1,5 +1,8 @@
 # Given a string s, find the length of the longest substring without repeating characters.
-class Solution:
+from turtle import right
+
+
+class SolutionBruteForce:
     def getLongestSequenceFromIndex(self, start_idx: int, s: str) -> int:
         seq_length = 0
         seen = []
@@ -21,7 +24,36 @@ class Solution:
                 longest_string_found = this_longest_sequence
         return longest_string_found
 
+class Solution:
+    def getNextLeftIteration(self, start_left_idx: int, input_string: str, duplicated_char: str):
+        leftmost_possible = (input_string[start_left_idx:]).index(duplicated_char) + 1 + start_left_idx
+        # while leftmost_possible < len(input_string) - 1 and input_string[leftmost_possible + 1] == duplicated_char:
+        #     leftmost_possible += 1
+        return leftmost_possible
+
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # Efficient approach
+        longest_string_found = 0
+        left_iter = right_iter = 0
+        
+
+        while left_iter < len(s):
+            seen = [] # Don't forget to pop duplicate out as moving left iter
+            right_iter = max(left_iter, right_iter)
+            while True:
+                if right_iter >= len(s):
+                    # Reached the end of the string
+                    return max(len(seen), longest_string_found)
+                if s[right_iter] in seen:
+                    # Found a duplicate so we need to save the longest string val and reorient the left side
+                    longest_string_found = max(len(seen), longest_string_found)
+                    left_iter = self.getNextLeftIteration(left_iter, s, s[right_iter])
+                    break
+                seen.append(s[right_iter])
+                right_iter += 1
+        return longest_string_found
+
 if __name__ == '__main__':
     x = Solution()
-    input_string = " "
+    input_string = "dvdf"
     print(x.lengthOfLongestSubstring(input_string))

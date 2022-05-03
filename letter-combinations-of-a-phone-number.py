e67148cf4844b5c69f19fc2cf7da7efb0602ab6b
Trevor Chinn <revolution.x@gmail.com>
2022-05-03 23:44:07 +0900

    Add a recursive version as well

--- letter-combinations-of-a-phone-number.py
+++ letter-combinations-of-a-phone-number.py
@@ -7,8 +7,9 @@ from typing import List
 
 
 class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        nums_to_letters = {
+    # default constructor
+    def __init__(self):
+        self.nums_to_letters = {
             "2": ["a", "b", "c"],
             "3": ["d", "e", "f"],
             "4": ["g", "h", "i"],
@@ -18,12 +19,34 @@ class Solution:
             "8": ["t", "u", "v"],
             "9": ["w", "x", "y", "z"],
         }
+
+    def getCombinedLettersRecurse(self, str_to_break_down: str, current_combinations: List[str]) -> List[str]:
+        if len(str_to_break_down) == 1:
+            return self.nums_to_letters[str_to_break_down]
+
+        # Combine current and new combos
+        this_output = []
+        new_combos = self.getCombinedLettersRecurse(str_to_break_down[1:], current_combinations)
+        for n in new_combos:
+            for c in self.nums_to_letters[str_to_break_down[:1]]:
+                this_output.append(c+n)
+        return this_output
+
+    # Recursive attempt
+    def letterCombinations(self, digits: str) -> List[str]:
+        if len(digits) == 0:
+            return []
+        output = self.getCombinedLettersRecurse(digits, [])
+        return output
+
+    def letterCombinationsIterative(self, digits: str) -> List[str]:
+        
         output = []
         # It feels like this could be a candidate for recursion?
         intermediate = []
         intermediate_idx = []
         for idx, d in enumerate(digits):
-            intermediate.append(nums_to_letters[d])
+            intermediate.append(self.nums_to_letters[d])
             intermediate_idx.append(0)
 
         intermediate_idx_incr = len(intermediate_idx) - 1
@@ -53,21 +76,24 @@ class Solution:
 
 
 if __name__ == '__main__':
+    # x = Solution()
+    # print(x.letterCombinationsIterative("4"))
+    # print(x.letterCombinationsIterative("34"))
+    # print(x.letterCombinationsIterative("234"))
+    # exit()
     test_cases = [
-        "3343",
         "23",
         "",
         "2",
     ]
     correct_answers = [
-        [],
         ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"],
         [],
         ["a", "b", "c"],
     ]
     x = Solution()
     for i, t in enumerate(test_cases):
-        my_answer = x.letterCombinations(t)
+        my_answer = sorted(x.letterCombinations(t))
         print(
-            f"{'Correct' if my_answer == correct_answers[i] else 'Wrong'} : {my_answer} ({correct_answers[i]})"
+            f"{'Correct' if my_answer == sorted(correct_answers[i]) else 'Wrong'} : {my_answer} ({sorted(correct_answers[i])})"
         )

9e2a9b7f0d257e5abe3871bad269983db90d3511
Trevor Chinn <revolution.x@gmail.com>
2022-05-06 16:45:35 +0900

    Clear out to start again

--- generate-parentheses.py
+++ generate-parentheses.py
@@ -1,28 +1,9 @@
 # Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 from typing import List
 class Solution:
-    def generateRecursive(self, n:int, current_list: List[str]) -> List[str]:
-        if n > 1:
-            recursive_output = self.generateRecursive(n - 1, current_list)
-            for i in range(len(recursive_output)):
-                v = recursive_output[i]
-                recursive_output[i] = (f"{v}()")
-                recursive_output.append(f"(){v}")
-                recursive_output.append(f"({v})")
-            return recursive_output
-
-        return ["()"]
-
-        return recursive_output
-    def generateParenthesis(self, n: int) -> List[str]:
-        # Pattern 1: fully embedded
-        # Pattern 2: fully side by side
-        # Pattern 3: all side by side except last
-        # Pattern 4: Mixed of the above pieces
-
-        output = self.generateRecursive(n, [])
-        # This definitely feels like a recursive item
-        return list(set(output))
+    def generateParenthesis(self, n):
+        
+        # Recursively backtrack for open and closing characters
 
 if __name__ == '__main__':
     test_cases = [
@@ -43,6 +24,3 @@ if __name__ == '__main__':
         print(
             f"{'Correct' if sorted(my_answer) == sorted(correct_answers[i]) else 'Wrong'} : {sorted(my_answer)} \033[1;32m ({sorted(correct_answers[i])})\u001b[37m"
         )
-# Missing from 4: '(())(())'
-# ['(((())))', '((()()))', '((())())', '((()))()', '(()(()))', '(()()())', '(()())()', '(())()()', '()((()))', '()(()())', '()(())()', '()()(())', '()()()()']
-# ['(((())))', '((()()))', '((())())', '((()))()', '(()(()))', '(()()())', '(()())()', '(())(())', '(())()()', '()((()))', '()(()())', '()(())()', '()()(())', '()()()()']

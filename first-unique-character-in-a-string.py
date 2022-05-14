2ec0ca0477f7418fa3abae4fe8131701aa79e29c
Trevor Chinn <trevor.chinn@jp.kpmg.com>
2022-05-14 16:35:22 +0900

    much better way

--- first-unique-character-in-a-string.py
+++ first-unique-character-in-a-string.py
@@ -4,15 +4,11 @@ Given a string s, find the first non-repeating character in it and return its in
 """
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        non_repeating_idx = None
-        char_idx = {}
+        char_count = {}
+        for c in s:
+            char_count[c] = char_count.get(c, 0) + 1
+
         for i, c in enumerate(s):
-            char_idx[c] = char_idx.get(c, []) + [i]
-
-
-        for c in char_idx:
-            if len(char_idx[c]) > 1:
-                continue
-            this_min = min(char_idx[c])
-            non_repeating_idx = this_min if None == non_repeating_idx else min(non_repeating_idx, this_min)
-        return -1 if None == non_repeating_idx else non_repeating_idx
+            if char_count[c] == 1:
+                return i
+        return -1

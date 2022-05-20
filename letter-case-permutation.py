8b9d7826f9d8ae59a353f114982e31bab3a3e260
Trevor Chinn <trevor.chinn@jp.kpmg.com>
2022-05-20 23:13:37 +0900

    Finished iterative but stole a recursive

--- letter-case-permutation.py
+++ letter-case-permutation.py
@@ -17,33 +17,43 @@ Output: ["3z4","3Z4"]
 import enum
 from typing import List
 class Solution:
+    def letterCasePermutationRecursive(self, s: str) -> List[str]:
+        str_len = len(s)
+        def helper(idx, substring):
+            if idx == str_len:
+                return [substring]
+
+            results = []
+
+            char = s[idx]
+            if char.isalpha():
+                if char.islower():
+                    results += helper(idx+1, substring+char.upper())
+                else:
+                    results += helper(idx+1, substring+char.lower())
+            results += helper(idx+1,substring+char)
+
+            return results
+
+        return helper(0, '')
+
     def letterCasePermutation(self, s: str) -> List[str]:
-        s = s.lower()
+        s = list(s.lower())
         letter_positions_to_permute = []
         for i, c in enumerate(s):
             if c.isdigit():
                 continue
             letter_positions_to_permute.append(i)
 
-        output = []
-        def caseHelper(permute_char_list:List[str], positions:List[int], existing_combos:List[List[str]]):
-            if not positions:
-                return existing_combos
-
-            this_iteration = []
-            for i, v in enumerate(positions):
-                items = caseHelper(permute_char_list, positions[i+1:], existing_combos + [permute_char_list])
-                updated = permute_char_list.copy()
-                updated[v] = updated[v].upper()
-                items += caseHelper(permute_char_list, positions[i+1:], existing_combos + [updated])
-                for c in items:
-                    print(c)
-            return this_iteration
-
-
-        output = caseHelper(list(s), letter_positions_to_permute, [])
-        return output
+        output = [s]
+        for pos in letter_positions_to_permute:
+            # append uppercase version of the current char mutation for each item already in output
+            for i in range(len(output)):
+                upper = output[i].copy()
+                upper[pos] = upper[pos].upper()
+                output.append(upper)
+        return ["".join(i) for i in output]
 
 x = Solution()
-print(x.letterCasePermutation("a1b2"))
-print(x.letterCasePermutation("3z4"))
+print(x.letterCasePermutationRecursive("a1b2"))
+print(x.letterCasePermutationRecursive("3z4"))

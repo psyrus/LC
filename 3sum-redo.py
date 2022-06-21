8f6bc64c6fcf40971d5bc4a78fdc13d755b24e8f
Trevor Chinn <trevor.chinn@jp.kpmg.com>
2022-06-21 14:53:33 +0900

    finished 3sum properly

--- 3sum-redo.py
+++ 3sum-redo.py
@@ -34,14 +34,34 @@ class Solution:
         len_nums = len(nums)
 
         #TODO: Need to implement this one again because I suck
+        for lower in range(len_nums - 2):
+            if nums[lower] > 0 or (lower > 0 and nums[lower] == nums[lower - 1]):
+                # If the lowest point is already over 0, no point in running it
+                # Also need to skip duplicate lower values so that they don't end up with duplicate outputs
+                continue
+            upper = len_nums - 1
+            mover = lower + 1
+            while mover < upper:
+                total = nums[lower] + nums[mover] + nums[upper]
+                if total == 0:
+                    output.append([nums[lower], nums[mover], nums[upper]])
+                    # To avoid duplicates, push mover right until the next value is not the same as the current value
+                    mover += 1
+
+                    while mover < upper and nums[mover] == nums[mover - 1]:
+                        mover += 1
+                elif total < 0:
+                    mover += 1
+                else:
+                    upper -= 1
 
         return output
 
 x = Solution()
 
+print(x.threeSum([-1,0,1,2,-1,-4]))
 print(x.threeSum([-2,0,1,1,2]))
 print(x.threeSum([0,0,0]))
-print(x.threeSum([-1,0,1,2,-1,-4]))
 print(x.threeSum([]))
 print(x.threeSum([0]))
 

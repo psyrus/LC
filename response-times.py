8656c1e3260d183e24a3394617df00b2437c7843
Trevor Chinn <trevor.chinn@jp.kpmg.com>
2022-07-19 09:01:10 +0900

    Add Response item

---
+++ response-times.py
@@ -0,0 +1,6 @@
+"""
+System tracks the response times for a page.
+
+At any given time we need to be able to take the average of the last 5 minutes of response times
+Do not need to store the historical response time data?
+"""
